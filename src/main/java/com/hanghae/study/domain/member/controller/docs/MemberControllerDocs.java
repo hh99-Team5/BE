771dcf98b230c36fda4486c09e7633e426a02479
openmpy <suhwan@kakao.com>
2024-03-12 19:21:36 +0900

    Feat: 회원 이메일 중복 검사 Swagger 추가

--- src/main/java/com/hanghae/study/domain/member/controller/docs/MemberControllerDocs.java
+++ src/main/java/com/hanghae/study/domain/member/controller/docs/MemberControllerDocs.java
@@ -1,7 +1,11 @@
 package com.hanghae.study.domain.member.controller.docs;
 
-import com.hanghae.study.domain.member.dto.MemberRequestDto;
-import com.hanghae.study.domain.member.dto.MemberResponseDto;
+import com.hanghae.study.domain.member.dto.MemberRequestDto.MemberCheckEmailRequestDto;
+import com.hanghae.study.domain.member.dto.MemberRequestDto.MemberSignupRequestDto;
+import com.hanghae.study.domain.member.dto.MemberRequestDto.MemberUpdateRequestDto;
+import com.hanghae.study.domain.member.dto.MemberResponseDto.MemberCheckEmailResponseDto;
+import com.hanghae.study.domain.member.dto.MemberResponseDto.MemberSignupResponseDto;
+import com.hanghae.study.domain.member.dto.MemberResponseDto.MemberUpdateResponseDto;
 import com.hanghae.study.global.dto.ResponseDto;
 import com.hanghae.study.global.security.UserDetailsImpl;
 import io.swagger.v3.oas.annotations.Operation;
@@ -14,13 +18,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 public interface MemberControllerDocs {
 
     @Operation(summary = "회원 가입 기능", description = "회원 가입할 수 있는 API")
-    ResponseDto<MemberResponseDto.MemberSignupResponseDto> signup(
-            @RequestBody @Valid MemberRequestDto.MemberSignupRequestDto requestDto
+    ResponseDto<MemberSignupResponseDto> signup(
+            @RequestBody @Valid MemberSignupRequestDto requestDto
     );
 
     @Operation(summary = "회원 정보 수정 기능", description = "회원 정보를 수정할 수 있는 API")
-    ResponseDto<MemberResponseDto.MemberUpdateResponseDto> editMember(
+    ResponseDto<MemberUpdateResponseDto> editMember(
             @AuthenticationPrincipal UserDetailsImpl userDetails,
-            @RequestBody @Valid MemberRequestDto.MemberUpdateRequestDto requestDto
+            @RequestBody @Valid MemberUpdateRequestDto requestDto
+    );
+
+    @Operation(summary = "회원 이메일 중복 검사 기능", description = "가입된 회원 이메일을 중복 검사할 수 있는 API")
+    ResponseDto<MemberCheckEmailResponseDto> checkEmail(
+            @RequestBody @Valid MemberCheckEmailRequestDto requestDto
     );
 }

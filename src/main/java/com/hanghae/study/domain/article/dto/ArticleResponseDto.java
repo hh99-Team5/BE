ada1d6722aa0a190a02c4cd954611cfd99654beb
VanMao <139448668+lsc713@users.noreply.github.com>
2024-03-12 21:03:33 +0900

    feat add #3 일지 CRUD 기능 구현

--- src/main/java/com/hanghae/study/domain/article/dto/ArticleResponseDto.java
+++ src/main/java/com/hanghae/study/domain/article/dto/ArticleResponseDto.java
@@ -1,4 +1,139 @@
 package com.hanghae.study.domain.article.dto;
 
+import com.hanghae.study.domain.article.entity.Article;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
 public class ArticleResponseDto {
+
+    public record CreateArticleResponseDto (
+        Long id,
+        String writer,
+        String title,
+        String contents,
+        LocalDateTime createdAt
+        ){
+
+        public CreateArticleResponseDto(Article article, String email) {
+            this(
+                    article.getId(),
+                    email,
+                    article.getTitle(),
+                    article.getContents(),
+                    LocalDateTime.now()
+            );
+        }
+    }
+
+    public record GetArticleResponseDto (
+        Long id,
+        String writer,
+        String title,
+        String contents,
+        Long likes,
+        LocalDateTime createdAt,
+        LocalDateTime modifiedAt){
+
+        public GetArticleResponseDto(Article article, Long likes) {
+            this(article.getId(),
+                    article.getMember().getEmail(),
+                    article.getTitle(),
+                    article.getContents(),
+                    likes,
+                    article.getCreatedAt(),
+                    article.getUpdatedAt()
+            );
+        }
+
+        public static GetArticleResponseDto from(Article article, Long likes) {
+            return new GetArticleResponseDto(
+                    article.getId(),
+                    article.getMember().getEmail(),
+                    article.getTitle(),
+                    article.getContents(),
+                    likes,
+                    article.getCreatedAt(),
+                    article.getUpdatedAt()
+            );
+        }
+    }
+
+    public record UpdateArticleResponseDto (
+        Long id,
+        String writer,
+        String title,
+        String contents,
+        LocalDateTime createdAt,
+        LocalDateTime modifiedAt){
+
+        public UpdateArticleResponseDto(Article article) {
+            this(article.getId(),
+            article.getMember().getEmail(),
+            article.getTitle(),
+            article.getContents(),
+            article.getCreatedAt(),
+            article.getUpdatedAt()
+        );
+        }
+
+
+    }
+
+//    @Getter
+//    public static class CreateArticleResponseDto {
+//        private Long id;
+//        private String writer;
+//        private String title;
+//        private String contents;
+//        private LocalDateTime createdAt;
+//
+//        public CreateArticleResponseDto(Article article) {
+//            this.id = article.getId();
+//            this.writer = article.getMember().getEmail();
+//            this.title = article.getTitle();
+//            this.contents = article.getContents();
+//            this.createdAt = LocalDateTime.now();
+//        }
+//    }
+//
+//    @Getter
+//    public static class GetArticleResponseDto {
+//        private Long id;
+//        private String writer;
+//        private String title;
+//        private String contents;
+//        private Long likes;
+//        private LocalDateTime createdAt;
+//        private LocalDateTime modifiedAt;
+//
+//        public GetArticleResponseDto(Article article, Long likes) {
+//            this.id = article.getId();
+//            this.writer = article.getMember().getEmail();
+//            this.title = article.getTitle();
+//            this.contents = article.getContents();
+//            this.likes = likes;
+//        }
+//    }
+//
+//    @Getter
+//    public static class UpdateArticleResponseDto {
+//        private Long id;
+//        private String writer;
+//        private String title;
+//        private String contents;
+//        private LocalDateTime createdAt;
+//        private LocalDateTime modifiedAt;
+//
+//        public UpdateArticleResponseDto(Article article) {
+//            this.id = article.getId();
+//            this.writer = article.getMember().getEmail();
+//            this.title = article.getTitle();
+//            this.contents = article.getContents();
+//            this.createdAt = article.getCreatedAt();
+//            this.modifiedAt = article.getModifiedAt();
+//        }
+//
+//
+//    }
 }
--- src/main/java/com/hanghae/study/global/exception/ErrorCode.java
+++ src/main/java/com/hanghae/study/global/exception/ErrorCode.java
@@ -9,6 +9,9 @@ public enum ErrorCode {
     ALREADY_EXIST_EMAIL("이미 존재하는 이메일입니다."),
     NOT_FOUND_EMAIL("존재하지 않는 이메일입니다."),
     NOT_FOUND_MEMBER("찾을 수 없는 회원입니다."),
+    NOT_FOUND_ARTICLE("찾을 수 없는 일지입니다."),
+    ARTICLE_NOT_MATCH_MEMBER("일지와 일치하는 회원이 없습니다."),
+    LIKE_NOT_FOUND("회원의 좋아요를 찾을 수 없습니다.")
     ;
 
     private final String message;
